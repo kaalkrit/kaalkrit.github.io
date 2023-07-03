5a5a453561df5395c2062d072a0d3f29a95462e6
Kaalkrit <i19ph002@phy.svnit.ac.in>
2023-07-03 13:32:58 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
+<script>
 $(document).ready(function(){
   $(".btn-primary").click(function(){
     $(".collapse").collapse('toggle');
   });
+  $(".btn-success").click(function(){
+    $(".collapse").collapse('show');
+  });
+  $(".btn-warning").click(function(){
+    $(".collapse").collapse('hide');
+  });
 });
-
+</script>
